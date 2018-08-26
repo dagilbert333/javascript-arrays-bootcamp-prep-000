b235ae5e0851596d532f262a74ba5e3f2781329e
David Gilbert <dagilbert333@gmail.com>
2018-08-26 01:05:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,4 +37,7 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
-function 
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
